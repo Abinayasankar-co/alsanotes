d04cf3302ce91cacea39cdbf12f5b6010d8f303a
Abinayasankar-co <abinayasankar@getmastery.ai>
2024-10-23 10:17:04 +0530

    Finalized Fusion Algo

--- backend/services/RAG.py
+++ backend/services/RAG.py
@@ -9,6 +9,9 @@ import os
 
 load_dotenv()
 
+#Initialy We have done Storing Chunks as Vector Embeds in Mongodb Atlas server and with the help of  $match and $vectorsearch we use to retrieve these chunks for further processing.
+#as a Finalized Approach we are dedicated to perform RAG Fusion and Ranking algo for these chunks and customize Optimability of these Visual Content Product
+
 class VisualContent:
     def __init__(self,pdf_docs:str) -> None:
        self.encoded_username = urllib.parse.quote_plus('ALSA_LOGIN_MANAGER')
@@ -25,9 +28,7 @@ class VisualContent:
         cursor = self.collection.list_search_indexes()
         for index in cursor:
            print(index)
-
-
-    
+   
     def get_pdf_text(self):
         try:
             if not self.pdf_docs or self.pdf_docs == ".":
@@ -65,6 +66,7 @@ class VisualContent:
             raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {e}")
     
     def query_results(self,query:str,user_id:int,name:str):
+        results_of_vector = []
         queryvector = self.embed_vector.llm_embedding(query)
         results = self.collection.aggregate(
             [
@@ -81,12 +83,15 @@ class VisualContent:
             'user_id': user_id,
             'name': name
         }
-    }
-]
+      }
+    ]
         )
         
         for document in results:
-            print(document)
+            resulted_chunks = document["chunk"]
+            results_of_vector.append(resulted_chunks)
+        
+        return {"user_id":user_id,"chunks":resulted_chunks}
 
 def main():
     try:
@@ -94,12 +99,10 @@ def main():
       user_id=102
       name="sankar"
       visualize = VisualContent(pdf_docs="../Output/SourceCode.pdf") 
-      visualize.get_pdf_text()
-      visualize.get_text_chunks()
-      result = visualize.storing_vectors(user_id=102,name="sankar")
-      result = visualize.query_results(query,user_id,name)
-      visualize.view_indexes()
-      print(result)
+      #visualize.get_pdf_text()
+      #visualize.get_text_chunks()
+      #result = visualize.storing_vectors(user_id=102,name="sankar")
+      visualize.query_results(query,user_id,name)
     except Exception as e:
        raise HTTPException(status_code=404,detail=f"Hello {e}")
 
